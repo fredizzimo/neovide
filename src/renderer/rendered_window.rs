ebf5ced250b1ec287bac3f9effe950b2a9341922
Fred Sundvik <fsundvik@gmail.com>
2022-10-02 15:25:12 +0300

    Split rendering and animation
    The animate function returs a boolean that tells if the animation should
    continue or not.

--- src/renderer/rendered_window.rs
+++ src/renderer/rendered_window.rs
@@ -9,7 +9,6 @@ use crate::{
     dimensions::Dimensions,
     editor::Style,
     profiling::tracy_zone,
-    redraw_scheduler::REDRAW_SCHEDULER,
     renderer::{animation_utils::*, GridRenderer, RendererSettings},
 };
 
@@ -143,7 +142,7 @@ impl RenderedWindow {
         Rect::from_point_and_size(current_pixel_position, image_size)
     }
 
-    pub fn update(&mut self, settings: &RendererSettings, dt: f32) -> bool {
+    pub fn animate(&mut self, settings: &RendererSettings, dt: f32) -> bool {
         let mut animating = false;
 
         {
@@ -265,12 +264,7 @@ impl RenderedWindow {
         settings: &RendererSettings,
         default_background: Color,
         font_dimensions: Dimensions,
-        dt: f32,
     ) -> WindowDrawDetails {
-        if self.update(settings, dt) {
-            REDRAW_SCHEDULER.queue_next_frame();
-        }
-
         let has_transparency = default_background.a() != 255 || self.has_transparency();
 
         let pixel_region = self.pixel_region(font_dimensions);
